ce18a56ff80ce34a815218aa3729029365286d22
Samuel Kortas <samuel.kortas@kaust.edu.sa>
2016-06-13 02:55:15 +0300

    adding license

--- env.py
+++ env.py
@@ -2,6 +2,20 @@
 #!/logiciels/public/bin/python-2.4
 #
 #
+# This file is part of Breakit.
+
+#  Breakit is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  Breakit is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Breakit.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 module finding the environment in where program is called
